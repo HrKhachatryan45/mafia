bdc2f8e1ac3ea4be2366d3b87b7f79d50ee7a35f
Your Name <you@example.com>
2025-01-20 20:27:05 +0400

    phase change

--- frontend/src/pages/Demo.js
+++ frontend/src/pages/Demo.js
@@ -209,7 +209,7 @@ function Demo(props) {
 
     useEffect(() => {
         setToNight(false)
-    },[message,mafiaMessage])
+    },[message,mafiaMessage,changedMessage])
 
     return (
 
@@ -257,7 +257,7 @@ function Demo(props) {
                                     (r) => r.playerId._id === authUser._id && r.role === "Mafia"
                                 );
                                 return (
-                                    <div className={"w-fit flex flex-col relative items-center justify-center "}
+                                    <div style={{minWidth:"50px"}} className={"w-fit  flex flex-col relative items-center justify-center "}
                                          key={role.playerId._id}>
                                             {authUser.isAdmin && room.doctorAction.isDone && role.role === "Doctor" &&
                                                 <h2 className={'   text-white  indicator-item badge badge-success z-50'}>
